4aedd4324b7da353dad3deb4d76be9b11c864393
lyudishu <lester.lyu@mail.utoronto.ca>
2017-10-16 00:43:54 -0400

    update tar.gz binary

--- config.json.js
+++ config.json.js
@@ -2,7 +2,7 @@
     "build_date": "2017-10-15_15:11:43", 
     "description": "ss-server", 
     "home_url": "Module_ssserver.asp", 
-    "md5": "0dca097ad2adf8fb2b5850d78380e61f", 
+    "md5": "f985af51db7150c9af62b6489eead480", 
     "module": "ssserver", 
     "title": "ss-server", 
     "version": "2.0"
